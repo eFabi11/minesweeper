9e309e4db0a59e3f4579d41c630b82b750264448
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2022-10-28 11:37:07 +0200

    FieldSpec change

--- src/main/scala/de/htwg/se/minesweeper/model/Field.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/Field.scala
@@ -5,12 +5,12 @@ import scala.io.StdIn._
 //case class Field()
 
 object Field {
-    def main(args:Array[String]) = {
+/*     def main(args:Array[String]) = {
         println("Welcome to Minesweeper \n")
         val salutation = "Hi " + signUp(args)
         println(salutation)
         showGrid()
-    }
+    } */
     // enters player name
     def signUp(playerNames:Array[String]):Person = {
         val name = if (playerNames.length > 0)
@@ -24,19 +24,19 @@ object Field {
     // test branch features creation
     // input number and shows grid
     def showGrid()= {
-/*         println("Enter size of grid: ")
+        println("Enter size of grid: ")
         val size = scala.io.StdIn.readInt()
         
-        println(mesh(size,size)) */
+        println(mesh(size,size)) 
         println(mesh(3,3))
     }
     //grid construction
     val eol = sys.props("line.separator")
-    def bar(cellWidth: Int = 5, cellNum: Int = 5): String = (("+" + "-" * 3) * cellNum) + "+" + eol
-    def bar(): String = (("+" + "-" * 3) * 3) + "+" + eol
-    def cells(cellWidth: Int=5, cellNum: Int=5): String = ("|" + " " * 3) * cellNum + "|" + eol
-    def cells(): String = ("|" + " " * 3) * 3 + "|" + eol
-    def mesh(cellWidth: Int=5, cellNum: Int=5): String =(bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
+    def bar(cellWidth: Int = 3, cellNum: Int = 3): String = (("+" + "-" * 3) * cellNum) + "+" + eol
+    //def bar(): String = (("+" + "-" * 3) * 3) + "+" + eol
+    def cells(cellWidth: Int=3, cellNum: Int=3): String = ("|" + " " * 3) * cellNum + "|" + eol
+    //def cells(): String = ("|" + " " * 3) * 3 + "|" + eol
+    def mesh(cellWidth: Int=3, cellNum: Int=3): String =(bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
 
 
 }
