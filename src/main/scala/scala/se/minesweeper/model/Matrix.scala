f6bd71305ca291aa21933e05541e26111b29c9db
Fabian EItel <fabian.eitel@yahoo.de>
2022-11-04 09:28:08 +0100

    Matrix creation class

---
+++ src/main/scala/scala/se/minesweeper/model/Matrix.scala
@@ -0,0 +1,10 @@
+package se.minesweeper.model
+
+case class Matrix[T](rows: Vector[Vector[T]]):
+    def this(size: Int, filling: T) = this(Vector.tabulate(size,size) {(row, col) => filling})
+    val size: Int = rows.size
+    def cells(row: Int, col: Int): T = rows(row)(col)
+    def row(row: Int) = rows(row)
+    def fill(filling: T): Matrix[T] = copy(Vector.tabulate(size,size) { (row, col) => filling})
+    def replaceCell(row: Int, col: Int, cell: T): Matrix[T] = copy(rows.updated(row, rows(row).updated(col, cell)))
+    
