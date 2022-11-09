97ff69657643536b3991f3ad9e740a82bbcaa16a
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2022-11-10 00:34:44 +0100

    MVC structure implementation

--- src/main/scala/se/minesweeper/Main.scala
+++ src/main/scala/se/minesweeper/Main.scala
@@ -1,5 +1,7 @@
 package htwg.se.minesweeper
 
+import se.minesweeper.aview.TUI
+import se.minesweeper.controller.Controller
 import se.minesweeper.model.Marker
 import se.minesweeper.model.Matrix
 import se.minesweeper.model.Field
@@ -8,29 +10,18 @@ import scala.io.StdIn._
 @main def run: Unit =
   println("Welcome to Minesweeper")
   val field = new Field(6, Marker.Empty)
-  println(field.toString)
-  getInputAndPrintLoop(field)
+  val controller = Controller(field)
+  val tui = TUI(controller)
+  tui.run
+  
+  
+  //println(field.toString)
+  //getInputAndPrintLoop(field)
 
-def getInputAndPrintLoop(field: Field): Unit =
-  val input = readLine
-  parseInput(input) match
-    case None => field
-    case Some(newfield) =>
-      println(newfield)
-      getInputAndPrintLoop(newfield)
-
-  def parseInput(input: String): Option[Field] =
-    input match
-      case "q" => None
-      case _ => {
-        val chars = input.toCharArray
-        val marker = chars(0) match
-          case 'F' => Marker.F
-          case 'f' => Marker.F
-          case 'O' => Marker.O
-          case 'o' => Marker.O
-          case _   => Marker.Empty
-        val x = chars(1).toString.toInt
-        val y = chars(2).toString.toInt
-        Some(field.put(marker, x, y))
-      }
+//def getInputAndPrintLoop(field: Field): Unit =
+  //val input = readLine
+  //parseInput(input) match
+    //case None => field
+    //case Some(newfield) =>
+      //println(newfield)
+      //getInputAndPrintLoop(newfield)
--- src/main/scala/se/minesweeper/aview/TUI.scala
+++ src/main/scala/se/minesweeper/aview/TUI.scala
@@ -1,2 +1,37 @@
-package se.minesweeper.aview
+package se.minesweeper
+package aview
+
+import controller.Controller
+import model.Marker
+import scala.io.StdIn.readLine
+import util.Observer
+
+class TUI(controller: Controller) extends Observer:
+    controller.add(this)
+    def run = 
+        println(controller.field.toString)
+        getInputAndPrintLoop()
+
+    override def update = println(controller.field.toString)
+        
+
+    def getInputAndPrintLoop(): Unit =
+        val input = readLine
+        input match
+            case "q" =>
+            case _ => {
+                val chars = input.toCharArray
+                val marker = chars(0) match
+                    case 'F' => Marker.F
+                    case 'f' => Marker.F
+                    case 'O' => Marker.O
+                    case 'o' => Marker.O
+                    case _   => Marker.Empty
+                val x = chars(1).toString.toInt
+                val y = chars(2).toString.toInt
+                controller.put(marker, x, y)
+                println(controller.toString)
+                getInputAndPrintLoop()
+            }
+        
 
